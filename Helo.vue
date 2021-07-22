ac729074bba5c174f4dc1d0678b56dd26f29396d
Byrce <562391567@qq.com>
2021-07-22 21:44:23 +0800

    ii

---
+++ Helo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="item in items" :key="item.id">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    New,
+  },
+  data() {
+    return {
+      key: value,
+    };
+  },
+  created() {},
+  computed: {
+    name() {
+      return this.data;
+    },
+  },
+  methods: {
+    name() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
